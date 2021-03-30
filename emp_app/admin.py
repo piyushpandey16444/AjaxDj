a027de96af2e3beef083beb850d37f6980d3f147
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-03-30 23:28:49 +0530

    models created.

--- emp_app/admin.py
+++ emp_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Office, Employee
 
-# Register your models here.
+admin.site.register(Office)
+admin.site.register(Employee)
--- emp_app/models.py
+++ emp_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Office(models.Model):
+    name = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
+
+
+class Employee(models.Model):
+    GENDER = (
+        ('male', 'Male'),
+        ('female', 'Female')
+    )
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
+    gender = models.CharField(max_length=50, choices=GENDER)
+    office = models.ForeignKey("emp_app.Office", on_delete=models.CASCADE)
